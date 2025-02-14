0ca12e317aa34f9f080d188fcd88b45b0e0de739
Umad Akram <152366259+AkramUmad572@users.noreply.github.com>
2025-02-13 21:41:31 -0500

    Update db.js
    Example of how to upload your MongoDb database

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,1 +1,2 @@
-module.exports = { URI: "mongodb+srv://{MongoDbDatabaseLink}" };
+module.exports = { URI: "mongodb+srv://{Username:Password}@cluster0.0f3pz.mongodb.net" };
+//await mongoose.connect('mongodb+srv://ahmedsheikh:Test123@cluster0.0f3pz.mongodb.net/');
